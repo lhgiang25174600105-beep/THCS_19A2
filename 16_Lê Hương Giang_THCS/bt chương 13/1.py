01bf4b467c0e200cf7fc7abf8900cbe6123dae2a
lhgiang25174600105-beep <lhgiang.25174600105@sv.uneti.edu.vn>
2025-12-28 21:12:20 +0700

    Delete 16_Lê Hương Giang_THCS/bt chương 13 directory

--- 16_Lê Hương Giang_THCS/bt chương 13/1.py
+++
@@ -1,8 +0,0 @@
-with open("vanban.txt", "w", encoding="utf-8") as f :
-    f.write(
-        "Python là một ngôn ngữ lập trình mạnh mẽ, dễ học và có nhiều ứng dụng. Nó được sử dụng rộng rãi trong phát triển web, khoa học dữ liệu, trí tuệ nhân tạo và tự động hóa. Cộng đồng Python rất lớn và hỗ trợ tuyệt vời, với nhiều thư viện phong phú để giải quyết mọi vấn đề."
-    )
-with open("vanban.txt", "r", encoding="utf-8") as f:
-    nd = f.read()
-so_tu = len(nd.split())
-print("Tống số từ trong tập tin là: ", so_tu)
--- 16_Lê Hương Giang_THCS/bt chương 13/3.py
+++
@@ -1,5 +0,0 @@
-ds = [1, 3, 5, 7, 9, 11, 12]
-with open("so_nguyen.txt", "w", encoding="utf-8") as f:
-    for so in ds:
-        f.write(str(so)+"\n")
-print("Đã ghi danh sách")
