1295958a0ba00a90db03b8487cbb8d3ddd8d89f5
lhgiang25174600105-beep <lhgiang.25174600105@sv.uneti.edu.vn>
2025-12-28 21:22:25 +0700

    Delete 16_Lê Hương Giang_THCS/BT chương 12 directory

--- 16_Lê Hương Giang_THCS/BT chương 12/bài 1/main.py
+++
@@ -1,6 +0,0 @@
-from hinh_hoc import chu_vi_hinh_vuong
-C_vuong = chu_vi_hinh_vuong(5)
-from hinh_hoc import chu_vi_hinh_tron
-C_tron = chu_vi_hinh_tron(3)
-print("Chu vi hình vuông là: ", C_vuong)
-print("Chi vi hình tròn là: ", C_tron)
--- 16_Lê Hương Giang_THCS/BT chương 12/bài 2/chuoi_utility.py
+++
@@ -1,8 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    chuoi_dao = " "
-    for i in chuoi:
-        chuoi_dao = i + chuoi_dao
-    return chuoi_dao
-def dem_so_tu(chuoi):
-    tu = chuoi.split()
-    return len(tu)
